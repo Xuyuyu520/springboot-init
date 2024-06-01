afed45944e0f462e6223e4f4960632e44a3884c3
风间 <norbocampasjb9657@gmail.com>
2024-06-01 11:57:39 +0800

    完整代码初始化

---
+++ src/main/java/com/xyc/springbootinit/mapper/PostThumbMapper.java
@@ -0,0 +1,16 @@
+package com.xyc.springbootinit.mapper;
+
+import com.xyc.springbootinit.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/xyc/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.xyc.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xyc.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
